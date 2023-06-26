ed5ee5c88b8993be1e347c345332067649a93a5d
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-06-26 21:46:06 +0300

    task 3 complite

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,38 @@
-import Notiflix from 'notiflix';
+import Notiflix, { Notify } from 'notiflix';
 
 const refs = {
-  btnSubmit: document.querySelector('button'),
   form: document.querySelector('.form'),
+  inputDelay: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
 }
 const dataObj = {};
-refs.btnSubmit.addEventListener('click', createPromise);
-refs.form.addEventListener('input', addData);
+refs.form.addEventListener('submit', runPromise);
+
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
   } else {
-    // Reject
+        reject({position, delay})
   }
+  }, delay)
+    }) 
 }
 
-function addData(e) {
-  dataObj[e.target.name] = e.target.value;
+function runPromise(evt) {
+  evt.preventDefault();
+  let iDelay = parseInt(refs.inputDelay.value);
+  const iStep = Number(refs.inputStep.value);
+  const iAmount = Number(refs.inputAmount.value);
+
+  for (let i = 1; i <= iAmount; i++) {
+    createPromise(i, iDelay)
+      .then(({ position, delay }) => { Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`) })
+      .catch(({ position, delay }) => { Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`) });
+    iDelay += iStep;
+  }
 }
